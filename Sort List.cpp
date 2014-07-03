ea17b11a298128a344d38ad5ec7a836604e9e6d3
yuhongliang <yuhongliang.work@gmail.com>
2014-07-03 09:55:50 +0800

    sort list
    提交sort list测试

---
+++ Sort List.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ struct ListNode {                          
+     int val;                               
+     ListNode *next;                        
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+ 
+ 
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        
+    }
+};
+
+int main()
+{
+	return 0;
+}
