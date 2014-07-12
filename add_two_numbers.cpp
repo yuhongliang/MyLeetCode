e4a56a6191324a460c2209e33bfd8b9de985ce44
root <mind2code@gmail.com>
2014-07-12 10:46:23 +0800

    add two numbers成功AC

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+using namespace std;
  //Definition for singly-linked list.
 struct ListNode 
 {
@@ -16,13 +17,58 @@ class Solution
 			ListNode ret(-1);
 			ListNode *cur_ret = &ret;
 			int carry = 0;
-			while(cur_L1 && cur_L2)
+			while(cur_L1 || cur_L2)
 			{
-				int sum = carry + cur_L1->val + cur_L2->val;
+				int val_1 = cur_L1 ? cur_L1->val : 0;
+				int val_2 = cur_L2 ? cur_L2->val : 0;
+				int sum = carry + val_1 + val_2;
 				carry = sum>=10 ? 1 : 0;
-				cur_ret->next = new ListNode(sumi%10);
+				cur_ret->next = new ListNode(sum%10);
 				cur_ret = cur_ret->next;
+				cur_L1 = cur_L1 ? cur_L1->next : 0;
+				cur_L2 = cur_L2 ? cur_L2->next : 0;
 			}
-			cur_ret->next = cur_L1 ? cur_L1 : cur_L2;
+			if(carry)
+			{
+				cur_ret->next = new ListNode(1);
+			}
+			return ret.next;
 		}
 };
+void printList(ListNode *list)
+{
+	ListNode * cur = list;
+	while(cur)
+	{
+		cout<<cur->val<<"->";
+		cur = cur->next;
+	}
+	cout<<"null"<<endl;
+}
+
+ListNode *makeList(int val[], int n)
+{
+	ListNode ret(-1);
+	ListNode *tmp;
+	for(int i=0; i<n; i++)
+	{
+		tmp = new ListNode(val[i]);
+		tmp->next = ret.next;
+		ret.next = tmp;
+	}
+	return ret.next;
+}
+
+int main()
+{
+	int a[] = {1,2,3,4,5,6,7,8,9};
+	int b[] = {9,8,7,6,5,4,3,2};
+	ListNode *list_1 = makeList(a,sizeof(a)/sizeof(int));
+	ListNode *list_2 = makeList(b,sizeof(b)/sizeof(int));
+	printList(list_1);
+	printList(list_2);
+	Solution so;
+	ListNode *ret = so.addTwoNumbers(list_1, list_2);
+	printList(ret);
+	return 1;
+}
