7e2a3c96a0167409b220ab6884aeb2b0699dce92
root <mind2code@gmail.com>
2014-07-17 14:34:41 +0800

    remove_duplicates_from_sorted_list_2 Accepted

---
+++ remove_duplicates_from_sorted_list_2.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution
+{
+	public:
+		ListNode *deleteDuplicates(ListNode *head)
+		{
+			ListNode ret(-1);
+			ret.next = head;
+			ListNode *pre = &ret;
+			ListNode *cur = head;
+			bool dup_flag = false;
+			while(cur && cur->next)
+			{
+				if(cur->next->val == cur->val)
+				{
+					dup_flag = true;
+					ListNode *del = cur->next;
+					cur->next = cur->next->next;
+					delete del;
+				}
+				else
+				{
+					if(dup_flag)
+					{
+						ListNode *del = cur;
+						pre->next = cur->next;
+						delete del;
+						dup_flag = false;
+						cur = pre->next;
+					}
+					else
+					{
+						pre = pre->next;
+						cur = cur->next;
+					}
+				}
+			}
+			if(dup_flag)
+			{
+				delete pre->next;
+				pre->next = 0;
+			}
+			return ret.next;
+		}
+};
+
+/****** test code ***********************/
+ListNode *makeList(int val[], int n)
+{
+	ListNode ret(-1);
+	ListNode *tmp;
+	for(int i=0; i<n; i++)
+	{
+		tmp = new ListNode(val[i]);
+		tmp->next = ret.next;
+		ret.next = tmp;
+	}
+	return ret.next;
+}
+void printList(ListNode *list)
+{
+	ListNode * cur = list;
+	while(cur)
+	{
+		cout<<cur->val<<"->";
+		cur = cur->next;
+	}
+	cout<<"null"<<endl;
+}
+int main()
+{
+	int a[] = {1,1,1,2,2,3,4,5,6,7,7,7};
+	ListNode *list = makeList(a,sizeof(a)/sizeof(int));
+	printList(list);
+	Solution so;
+	printList(so.deleteDuplicates(list));
+	return 0;
+}
