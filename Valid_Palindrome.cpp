c7daf1b1a4e4e72eee0a52a0f40f6f6c43d2ed66
root <mind2code@gmail.com>
2014-07-29 19:35:23 +0800

    Valid_Palindrome Accepted

---
+++ Valid_Palindrome.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+using namespace std;
+class Solution 
+{
+	public:
+	    bool isPalindrome(string s) 
+		{
+			transform(s.begin(), s.end(), s.begin(), ::tolower);
+			auto left = s.begin(), right = prev(s.end());
+			while(left < right)
+			{
+				if(!isalnum(*left))
+					left++;
+				else if(!isalnum(*right))
+					right--;
+				else if(*left != *right)
+					return false;
+				else
+				{
+					left++;right--;
+				}
+			}
+			return true;
+	    }
+};
+
+int main()
+{
+	Solution so;
+	string str = "race a car";
+	cout<<so.isPalindrome(str)<<endl;
+	return 0;
+}
